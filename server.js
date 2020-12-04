16b53cc934eaa341bd13b9d493716846b6b9052d
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2020-12-03 22:07:50 -0300

    Correcoes

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/appCip'));
+app.use(express.static(__dirname + '/dist/web-parts'));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/appCip/index.html')); 
+    res.sendFile(path.join(__dirname + '/dist/web-parts/index.html')); 
 });
 
 app.listen(process.env.PORT || 8080);
